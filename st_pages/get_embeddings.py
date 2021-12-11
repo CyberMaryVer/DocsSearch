4795e59f615472eff5c6b08e61827cba34af9e11
CyberMaryVer <maria.s.startseva@gmail.com>
2021-12-12 00:07:04 +0200

    Initial commit

--- st_pages/get_embeddings.py
+++ st_pages/get_embeddings.py
@@ -1,4 +1,5 @@
 import requests
+import subprocess
 import os
 
 EMBEDDINGS = "./../data/embeddings.pk" if __name__ == "__main__" else "./data/embeddings.pk"
@@ -37,6 +38,7 @@ def download_file_from_google_drive(id, destination):
 def get_them_all():
     if not os.path.exists(EMBEDDINGS):
         download_file_from_google_drive("1Fjk6zk6qikPwv8ihjHP4LtZSExVfSD4h", EMBEDDINGS)
+    subprocess.run("python -m spacy download ru_core_news_md")
     print("Embeddings are here")
 
 
