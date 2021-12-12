d0d824585ed62d9c5d93c69456e61a6dd6456095
CyberMaryVer <maria.s.startseva@gmail.com>
2021-12-12 14:07:15 +0200

    Update

--- st_pages/st_team.py
+++ st_pages/st_team.py
@@ -13,11 +13,11 @@ def st_team():
         st.info("Павел - дата-аналитик")
     with col2:
         # st_img("./data/tg.png")
-        st.markdown(f""":package: [Мария](https://t.me/cybermary)""",
+        st.markdown(f""":woman: [Мария](https://t.me/cybermary)""",
                     unsafe_allow_html=True)
-        st.markdown(f""":package: [Алина](https://t.me/alinka27_official)""",
+        st.markdown(f""":woman: [Алина](https://t.me/alinka27_official)""",
                     unsafe_allow_html=True)
-        st.markdown(f""":package: [Максим](https://t.me/Makcimilian)""",
+        st.markdown(f""":man: [Максим](https://t.me/Makcimilian)""",
                     unsafe_allow_html=True)
-        st.markdown(f""":package: [Павел](https://t.me/pavel_grom_gq)""",
+        st.markdown(f""":man: [Павел](https://t.me/pavel_grom_gq)""",
                     unsafe_allow_html=True)
